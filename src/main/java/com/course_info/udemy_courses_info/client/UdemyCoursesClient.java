4d0ecc7b5d76e91b0bd4e1e0e3088c4cc98e4111
Hlib <gfietisov@gmail.com>
2023-11-19 10:43:12 +0200

    update feign client. Add requestParams to retreive certain fields from UdemyAPI

--- src/main/java/com/course_info/udemy_courses_info/client/UdemyCoursesClient.java
+++ src/main/java/com/course_info/udemy_courses_info/client/UdemyCoursesClient.java
@@ -1,23 +1,39 @@
 package com.course_info.udemy_courses_info.client;
 
 import com.course_info.udemy_courses_info.config.FeignClientConfig;
-import com.course_info.udemy_courses_info.entity.Course;
+import com.course_info.udemy_courses_info.entity.BunchOfCoursesRequest;
+import com.course_info.udemy_courses_info.entity.GeneralDiscountCourseInfo;
+import com.course_info.udemy_courses_info.entity.PricingResult;
+import com.course_info.udemy_courses_info.entity.DetailedCourseInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @FeignClient(
-        name = "udemy-api-courses-info",
-        url = "https://www.udemy.com/api-2.0/courses",
+        name = "${feign.client.name}",
+        url = "${feign.client.udemy.api.url}",
         configuration = FeignClientConfig.class
 )
 public interface UdemyCoursesClient{
 
-    @GetMapping(value = "/{courseId}", produces = "application/json")
-    Course certainCourseInfo(@PathVariable String courseId);
+    @GetMapping(value = "courses/{courseId}", produces = "application/json")
+    DetailedCourseInfo getCertainCourseInfo(@PathVariable String courseId, @RequestParam("fields[course]") String requiredFields);
 
-    @GetMapping(value = "?page_size=1", produces = "application/json", headers = "Accept-Language: en-US")
-    String getBunchOfCourses(@RequestHeader("Authorization") String authorizationToken);
+    // TODO: parse JSON response properly
+    @GetMapping(value = "pricing")
+    GeneralDiscountCourseInfo getCourseDiscountPrice(@RequestParam("course_ids") String courseIds);
+
+    @GetMapping(value = "courses?page_size=10", produces = "application/json")
+    BunchOfCoursesRequest getBunchOfCourses(@RequestHeader("Authorization") String authorizationToken, @RequestParam("fields[course]") String requiredFields);
+
+    //TODO: figure out data amount for one page
+    @GetMapping(value = "courses?search={areaName}", produces = "application/json")
+    BunchOfCoursesRequest getSpecifiedAreaCourses(
+            @PathVariable String areaName,
+            @RequestHeader("Authorization") String authorizationToken,
+            @RequestParam("fields[course]") String requiredFields
+            );
 }
