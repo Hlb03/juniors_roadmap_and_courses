a737d57c908bf6e21f51e4733c42d73fab76ac54
Hlib <gfietisov@gmail.com>
2023-11-19 10:49:39 +0200

    add paths to swagger-ui and api-docs documentation + provide basic project description

--- src/main/java/com/course_info/udemy_courses_info/UdemyCoursesInfoApplication.java
+++ src/main/java/com/course_info/udemy_courses_info/UdemyCoursesInfoApplication.java
@@ -1,15 +1,25 @@
 package com.course_info.udemy_courses_info;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(
+        info = @Info(
+                title = "From start to success (FS2S)",
+                description = "This is a backend part of the application. This part is responsible for getting/processing courses from UdemyAPI and return them in another-structured form."
+        )
+)
 public class UdemyCoursesInfoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UdemyCoursesInfoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyCoursesInfoApplication.class, args);
+    }
 
 }
