9ca4fd069add80e8e27f446e8f2a571e0dda3469
Hlib <gfietisov@gmail.com>
2023-11-29 13:56:25 +0200

    add debug property to trace 3rd party api requests (udemyAPI)

---
+++ src/main/java/com/course_info/udemy_courses_info/entity/discount_price/DiscountedPrice.java
@@ -0,0 +1,7 @@
+package com.course_info.udemy_courses_info.entity;
+
+public class DiscountedPrice extends AbstractDiscountPrice {
+    public DiscountedPrice(String amount, String currency, String price_string) {
+        super(amount, currency, price_string);
+    }
+}
