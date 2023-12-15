5e938ca5ae39501d0a1f01c6ce73412ddd191aaf
Hlib <gfietisov@gmail.com>
2023-12-15 10:22:51 +0200

    update project swagger description

--- src/main/java/com/course_info/udemy_courses_info/UdemyCoursesInfoApplication.java
+++ src/main/java/com/course_info/udemy_courses_info/UdemyCoursesInfoApplication.java
@@ -3,10 +3,9 @@ package com.course_info.udemy_courses_info;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
 
 @SpringBootApplication
 @EnableFeignClients
